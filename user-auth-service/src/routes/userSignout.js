5d7ffef15580909eb25d691fa5c77639c37462d2
Sayeed Safvan <sayeedsafvan123@gmail.com>
2024-04-20 10:59:13 +0530

    fixed the mistaken configuratino

--- user-auth-service/src/routes/userSignout.js
+++ user-auth-service/src/routes/userSignout.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+
 router.post('/api/users/signout',async(req,res)=>{
           req.session = null 
           // res.clearCookie('jwt'); 
